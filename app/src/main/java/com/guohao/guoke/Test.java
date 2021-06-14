073e6f20aa5bf0ee437ff80b99bec396ca649e0d
guohao <guoke24@qq.com>
2021-06-14 19:28:42 +0800

    RecyclerView 和 Glide 的简单测试代码

--- app/src/main/java/com/guohao/guoke/Test.java
+++ app/src/main/java/com/guohao/guoke/Test.java
@@ -1,13 +1,17 @@
 package com.guohao.guoke;
 
+import android.app.Activity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class Test {
 
     public static void main(String[] args){
-        //Glide.with(this).load("").into(new View(this));
+        Glide.with(new Activity())
+                .load("")
+                .into(new ImageView(new Activity()));
     }
 
 }
